48a3b63c044f1a1ee3dacf0fa3290538e045ebab
rocod <rominacodarin@gmail.com>
2022-05-26 11:42:15 -0300

    migración datos contacto

--- database/migrations/2022_01_27_114708_create_datos_contactos_table.php
+++ database/migrations/2022_01_27_114708_create_datos_contactos_table.php
@@ -15,6 +15,11 @@ class CreateDatosContactosTable extends Migration
     {
         Schema::create('datos_contactos', function (Blueprint $table) {
             $table->id();
+            $table->string('seccion');
+            $table->string('linea1');
+            $table->string('linea2');
+            $table->string('linea3');
+            $table->string('linea4');
             $table->timestamps();
         });
     }
